faab835080c044a098932bcb0fa3a971ae669511
o-pylypiv <bassenia@gmail.com>
2024-06-04 18:38:53 +0300

    basic functionality

---
+++ Project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Olha Pylypiv on 12.03.2024.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
